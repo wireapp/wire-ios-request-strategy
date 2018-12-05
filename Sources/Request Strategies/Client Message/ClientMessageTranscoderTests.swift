47e57219ac7de55eaf6626f3e5cd8d55f4629217
Jacob Persson <jacob.persson@gmail.com>
2018-12-05 08:53:07 +0100

    Set the expectReadConfirmation flag also for asset messages

--- Sources/Request Strategies/Client Message/ClientMessageTranscoderTests.swift
+++ Sources/Request Strategies/Client Message/ClientMessageTranscoderTests.swift
@@ -168,6 +168,27 @@ extension ClientMessageTranscoderTests {
         }
     }
     
+    func testThatItDoesntUpdateExpectsReadConfirmationFlagWhenReadReceiptsAreDisabled() {
+        self.syncMOC.performGroupedBlockAndWait {
+            
+            // GIVEN
+            ZMUser.selfUser(in: self.syncMOC).readReceiptsEnabled = false
+            let text = "Lorem ipsum"
+            let message = self.oneToOneConversation.append(text: text) as! ZMClientMessage
+            self.syncMOC.saveOrRollback()
+            
+            // WHEN
+            self.sut.contextChangeTrackers.forEach { $0.objectsDidChange(Set([message])) }
+            if self.sut.nextRequest() == nil {
+                XCTFail()
+                return
+            }
+            
+            // THEN
+            XCTAssertFalse(message.genericMessage!.content!.expectsReadConfirmation())
+        }
+    }
+    
     func testThatItGeneratesARequestToSendAClientMessage() {
         self.syncMOC.performGroupedBlockAndWait {
             
