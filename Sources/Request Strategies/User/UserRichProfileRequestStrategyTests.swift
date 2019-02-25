58a0dbee81d5f04464abdd1caddc14ec9c2fce5e
Vytis Šibonis <vytis@wire.com>
2019-02-25 14:54:41 +0100

    Added few tests

--- Sources/Request Strategies/User/UserRichProfileRequestStrategyTests.swift
+++ Sources/Request Strategies/User/UserRichProfileRequestStrategyTests.swift
@@ -21,13 +21,67 @@ import XCTest
 
 class UserRichProfileRequestStrategyTests: MessagingTestBase {
 
+    var applicationStatus: MockApplicationStatus!
+    var sut : UserRichProfileRequestStrategy!
+    
     override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        super.setUp()
+        
+        self.syncMOC.performGroupedAndWait { moc in
+            self.applicationStatus = MockApplicationStatus()
+            self.applicationStatus.mockSynchronizationState = .eventProcessing
+            self.sut = UserRichProfileRequestStrategy(withManagedObjectContext: moc, applicationStatus: self.applicationStatus)
+        }
     }
-
+    
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        sut = nil
+        applicationStatus = nil
+        
+        super.tearDown()
     }
-
-
+    
+    func testThatItGeneratesARequestWhenSettingIsModified() {
+        self.syncMOC.performGroupedAndWait { moc in
+            // given
+            let userID = UUID()
+            let user = ZMUser(remoteID: userID, createIfNeeded: true, in: self.syncMOC)!
+            user.needsRichProfileUpdate = true
+            self.sut.contextChangeTrackers.forEach({ $0.addTrackedObjects(Set<NSManagedObject>(arrayLiteral: user)) })
+            
+            // when
+            guard let request = self.sut.nextRequest() else { XCTFail(); return }
+            
+            // then
+            XCTAssertEqual(request.path, "/users/\(userID)/rich_info")
+            XCTAssertEqual(request.method, .methodGET)
+        }
+    }
+    
+    func testThatItParsesAResponse() {
+        self.syncMOC.performGroupedAndWait { moc in
+            // given
+            let userID = UUID()
+            let user = ZMUser(remoteID: userID, createIfNeeded: true, in: self.syncMOC)!
+            user.needsRichProfileUpdate = true
+            self.sut.contextChangeTrackers.forEach({ $0.addTrackedObjects(Set<NSManagedObject>(arrayLiteral: user)) })
+            let request = self.sut.nextRequest()
+            XCTAssertNotNil(request)
+            
+            // when
+            let type = "some"
+            let value = "value"
+            let payload = [
+                "fields" : [
+                    ["type" : type, "value" : value]
+                ]
+            ]
+            let response = ZMTransportResponse(payload: payload as NSDictionary as ZMTransportData, httpStatus: 200, transportSessionError: nil)
+            self.sut.update(user, with: response, downstreamSync: nil)
+            
+            // then
+            XCTAssertFalse(user.needsRichProfileUpdate)
+            XCTAssertEqual(user.richProfile, [ZMUser.RichProfileField(type: type, value: value)])
+        }
+    }
 }
