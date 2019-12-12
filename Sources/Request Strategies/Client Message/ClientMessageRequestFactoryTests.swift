47cb018f0730b9bf63b8db4e88179fa26e4ebac8
Marco Conti <marcoconti83@gmail.com>
2019-12-12 17:56:48 +0100

    Remove test prints

--- Sources/Request Strategies/Client Message/ClientMessageRequestFactoryTests.swift
+++ Sources/Request Strategies/Client Message/ClientMessageRequestFactoryTests.swift
@@ -67,8 +67,6 @@ extension ClientMessageRequestFactoryTests {
             let confirmation = ZMConfirmation.confirm(messageId: message.nonce!, type: .DELIVERED)
             let confirmationMessage = self.oneToOneConversation.appendClientMessage(with: ZMGenericMessage.message(content: confirmation), expires: false, hidden: true)!
             
-            print("CLIENT ID", message.conversation?.localParticipants.first?.remoteIdentifier ?? "no ID")
-            print("OTHER USER", self.otherUser.remoteIdentifier!)
             // WHEN
             guard let request = ClientMessageRequestFactory().upstreamRequestForMessage(confirmationMessage, forConversationWithId: self.oneToOneConversation.remoteIdentifier!) else {
                 return XCTFail("No request")
