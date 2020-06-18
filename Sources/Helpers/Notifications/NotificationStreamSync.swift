26cd313db85c02a9145b9144d1efb0cf84acae41
KaterinaWire <katerina@wire.com>
2020-06-18 12:00:26 +0200

    Add UpdateEventProcessor protocol

--- Sources/Helpers/Notifications/NotificationStreamSync.swift
+++ Sources/Helpers/Notifications/NotificationStreamSync.swift
@@ -19,8 +19,15 @@
 import Foundation
 
 /// Holds a list of received event IDs
-@objc public protocol PreviouslyReceivedEventIDsCollection: NSObjectProtocol {
+@objc
+public protocol PreviouslyReceivedEventIDsCollection: NSObjectProtocol {
     func discardListOfAlreadyReceivedPushEventIDs()
+}
+
+@objc
+public protocol UpdateEventProcessor: class {
+    @objc(processUpdateEvents:ignoreBuffer:)
+    func process(updateEvents: [ZMUpdateEvent], ignoreBuffer: Bool)
 }
 
 public protocol NotificationStreamSyncDelegate: class {
