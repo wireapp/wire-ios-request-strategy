da3ee688a9bff5c0ea16bd40c91fcc9eaf9a25dd
KaterinaWire <57407805+KaterinaWire@users.noreply.github.com>
2020-11-19 15:18:58 +0100

    [Feature] Add FeatureConfigRequestStrategy (#255)
    * Draft FeatureConfigRequestStrategy class
    
    * Process response
    
    * Remove AppLock
    
    * Add FeatureConfigController
    
    * Update FeatureConfigController
    
    * Add FeatureController
    
    * Update FeatureConfigRequestStrategy
    
    * Update Cartfile
    
    * Add tests
    
    * Add featureController to the FeatureConfigRequestStrategy
    
    * Add tests
    
    * Fixes regarding comments
    
    * Fix tests
    
    * Fix tests
    
    * Add Named protocol
    
    * Temp comments
    
    * Move decoding data to the request strategy
    
    * Modified didReceive
    
    * Add pendingItems
    
    * Clean up
    
    * Make functions as internal
    
    * Fix tests
    
    * Clean up
    
    * Add comments
    
    * Fix tests
    
    * Removed CodingKeys
    
    * Move enum AppLock to the Feature extension
    
    * Clean up
    
    * Update according to comments
    
    * Update comments
    
    * Clean up
    
    * Add testThatItConsumesPendingItems test
    
    * Modify request(for sync: ZMSingleRequestSync)
    
    * clean up

---
+++ Sources/Request Strategies/Feature configurations/FeatureConfigRequestStrategy.swift
@@ -0,0 +1,147 @@
+//
+// Wire
+// Copyright (C) 2020 Wire Swiss GmbH
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see http://www.gnu.org/licenses/.
+//
+
+import Foundation
+
+@objcMembers
+public final class FeatureConfigRequestStrategy: AbstractRequestStrategy {
+    
+    enum PendingItem {
+        case singleFeature(name: String)
+        case allFeatures
+    }
+    
+    public static let needsToFetchFeatureConfigNotificationName = Notification.Name("needsToFetchFeatureConfiguration")
+    
+    private let zmLog = ZMSLog(tag: "feature configurations")
+    
+    private var observerToken: Any?
+    private var pendingItems: [PendingItem] = []
+    private var fetchSingleConfigSync: ZMSingleRequestSync!
+    private var fetchAllConfigsSync: ZMSingleRequestSync!
+    private var featureController: FeatureController!
+    
+    // MARK: - Init
+    public override init(withManagedObjectContext managedObjectContext: NSManagedObjectContext,
+                         applicationStatus: ApplicationStatus) {
+        
+        super.init(withManagedObjectContext: managedObjectContext, applicationStatus: applicationStatus)
+        
+        self.configuration = [.allowsRequestsWhileOnline,
+                              .allowsRequestsDuringQuickSync,
+                              .allowsRequestsWhileInBackground]
+        
+        self.featureController = FeatureController(managedObjectContext: managedObjectContext)
+        self.fetchSingleConfigSync = ZMSingleRequestSync(singleRequestTranscoder: self,
+                                                         groupQueue: managedObjectContext)
+        self.fetchAllConfigsSync = ZMSingleRequestSync(singleRequestTranscoder: self,
+                                                       groupQueue: managedObjectContext)
+        self.observerToken = NotificationInContext.addObserver(
+            name: FeatureConfigRequestStrategy.needsToFetchFeatureConfigNotificationName,
+            context: self.managedObjectContext.notificationContext,
+            object: nil) { [weak self] in
+                self?.requestConfig(with: $0)
+        }
+    }
+    
+    // MARK: - Overrides
+    public override func nextRequestIfAllowed() -> ZMTransportRequest? {
+        return fetchSingleConfigSync.nextRequest() ?? fetchAllConfigsSync.nextRequest()
+    }
+}
+
+// MARK: - ZMSingleRequestTranscoder
+extension FeatureConfigRequestStrategy: ZMSingleRequestTranscoder {
+    public func request(for sync: ZMSingleRequestSync) -> ZMTransportRequest? {
+        guard !pendingItems.isEmpty else {
+            return nil
+        }
+        let pendingItem = pendingItems.removeFirst()
+        switch (sync, pendingItem) {
+        case (fetchSingleConfigSync, let .singleFeature(name: featureName)):
+            return fetchConfigRequestFor(featureName)
+        case (fetchAllConfigsSync, .allFeatures):
+            return fetchAllConfigsRequest()
+        default:
+            return nil
+        }
+    }
+    
+    public func didReceive(_ response: ZMTransportResponse, forSingleRequest sync: ZMSingleRequestSync) {
+        guard response.result == .success else {
+            zmLog.error("error downloading feature configuration (\(response.httpStatus))")
+            return
+        }
+        guard let responseData = response.rawData else {
+            zmLog.error("response has no rawData")
+            return
+        }
+        
+        switch sync {
+        case fetchSingleConfigSync:
+            do {
+                //TODO Katerina make it more general for all kind of features
+                let configuration = try JSONDecoder().decode(FeatureConfigResponse<Feature.AppLock>.self, from: responseData)
+                featureController.save(Feature.AppLock.self, configuration: configuration)
+            } catch {
+                zmLog.error("Failed to decode feature config response: \(error)")
+            }
+        case fetchAllConfigsSync:
+            do {
+                let allConfigs = try JSONDecoder().decode(AllFeatureConfigsResponse.self, from: responseData)
+                featureController.saveAllFeatures(allConfigs)
+            } catch {
+                zmLog.error("Failed to decode feature config response: \(error)")
+            }
+        default:
+            break
+        }
+    }
+}
+
+// MARK: - Fetch configurations
+extension FeatureConfigRequestStrategy {
+    private func fetchAllConfigsRequest() -> ZMTransportRequest? {
+        guard let teamId = ZMUser.selfUser(in: managedObjectContext).teamIdentifier?.uuidString else {
+            return nil
+        }
+        return ZMTransportRequest(getFromPath: "/teams/\(teamId)/features")
+    }
+    
+    private func fetchConfigRequestFor(_ feature: String) -> ZMTransportRequest? {
+        guard let teamId = ZMUser.selfUser(in: managedObjectContext).teamIdentifier?.uuidString else {
+            return nil
+        }
+        return ZMTransportRequest(getFromPath: "/teams/\(teamId)/features/\(feature)")
+    }
+}
+
+// MARK: - Private methods
+extension FeatureConfigRequestStrategy {
+    private func requestConfig(with note: NotificationInContext) {
+        if let featureName = note.object as? String {
+            pendingItems.append(.singleFeature(name: featureName))
+            fetchSingleConfigSync.readyForNextRequestIfNotBusy()
+        } else {
+            pendingItems.append(.allFeatures)
+            fetchAllConfigsSync.readyForNextRequestIfNotBusy()
+        }
+        RequestAvailableNotification.notifyNewRequestsAvailable(self)
+    }
+}
+
---
+++ Sources/Request Strategies/Feature configurations/FeatureConfigRequestStrategyTests.swift
@@ -0,0 +1,132 @@
+//
+// Wire
+// Copyright (C) 2020 Wire Swiss GmbH
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see http://www.gnu.org/licenses/.
+//
+
+import XCTest
+@testable import WireRequestStrategy
+
+class FeatureConfigRequestStrategyTests: MessagingTestBase {
+    var mockApplicationStatus: MockApplicationStatus!
+    var sut: FeatureConfigRequestStrategy!
+    var selfUser: ZMUser!
+    
+    override func setUp() {
+        super.setUp()
+        mockApplicationStatus = MockApplicationStatus()
+        mockApplicationStatus.mockSynchronizationState = .slowSyncing
+        
+        RequestAvailableNotification.notifyNewRequestsAvailable(self)
+        sut = FeatureConfigRequestStrategy(withManagedObjectContext: syncMOC,
+                                           applicationStatus: mockApplicationStatus)
+        self.syncMOC.performGroupedAndWait { moc in
+            let team = Team.insertNewObject(in: moc)
+            team.name = "Wire Amazing Team"
+            team.remoteIdentifier = UUID.create()
+            self.selfUser = ZMUser.selfUser(in: moc)
+            self.selfUser.teamIdentifier = team.remoteIdentifier
+        }
+    }
+    
+    override func tearDown() {
+        mockApplicationStatus = nil
+        sut = nil
+        selfUser = nil
+        super.tearDown()
+    }
+    
+    // MARK: Request generation
+    func testThatItGeneratesARequestToFetchAllFeatureConfigurations() {
+        self.syncMOC.performGroupedAndWait { moc in
+            // given
+            NotificationInContext(name: FeatureConfigRequestStrategy.needsToFetchFeatureConfigNotificationName,
+                                  context: moc.notificationContext,
+                                  object: nil).post()
+            
+            // when
+            guard let teamId = self.selfUser.teamIdentifier?.uuidString,
+                let request = self.sut.nextRequestIfAllowed() else {
+                    XCTFail()
+                    return
+            }
+            
+            // then
+            XCTAssertEqual(request.path, "/teams/\(teamId)/features")
+        }
+    }
+    
+    func testThatItGeneratesARequestToFetchASingleFeatureConfiguration() {
+        self.syncMOC.performGroupedAndWait { moc in
+            // given
+            NotificationInContext(name: FeatureConfigRequestStrategy.needsToFetchFeatureConfigNotificationName,
+                                  context: moc.notificationContext,
+                                  object: "applock" as AnyObject).post()
+            
+            // when
+            guard let teamId = self.selfUser.teamIdentifier?.uuidString,
+                let request = self.sut.nextRequestIfAllowed() else {
+                    XCTFail()
+                    return
+            }
+            
+            // then
+            XCTAssertEqual(request.path, "/teams/\(teamId)/features/applock")
+        }
+    }
+    
+    func testThatItDoesNotGenerateARequestForNonTeamUser() {
+        self.syncMOC.performGroupedAndWait { moc in
+            // given
+            NotificationInContext(name: FeatureConfigRequestStrategy.needsToFetchFeatureConfigNotificationName,
+                                  context: moc.notificationContext,
+                                  object: nil).post()
+            self.selfUser.teamIdentifier = nil
+            
+            // when
+            let request = self.sut.nextRequestIfAllowed()
+            
+            // then
+            XCTAssertNil(request)
+        }
+    }
+    
+    func testThatItDoesNotGenerateARequestIfThereAreNoPendingItems() {
+        self.syncMOC.performGroupedAndWait { moc in
+            // when
+            let request = self.sut.nextRequestIfAllowed()
+            
+            // then
+            XCTAssertNil(request)
+        }
+    }
+    
+    func testThatItConsumesPendingItems() {
+        self.syncMOC.performGroupedAndWait { moc in
+            // given
+            NotificationInContext(name: FeatureConfigRequestStrategy.needsToFetchFeatureConfigNotificationName,
+                                  context: moc.notificationContext,
+                                  object: nil).post()
+            
+            // when
+            let firstRequest = self.sut.nextRequestIfAllowed()
+            XCTAssertNotNil(firstRequest)
+            let secondRequest = self.sut.nextRequestIfAllowed()
+            
+            // then
+            XCTAssertNil(secondRequest)
+        }
+    }
+}
