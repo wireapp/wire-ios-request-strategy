634a8b1620fec530471f808d139139bdceaf2925
Bill Chan <billypchan@gmail.com>
2021-06-04 10:14:26 +0200

    fix test crash

--- Sources/Helpers/ZMTBaseTests+CoreDataStack.swift
+++ Sources/Helpers/ZMTBaseTests+CoreDataStack.swift
@@ -44,7 +44,7 @@ extension ZMTBaseTest {
     @objc
     func setupCaches(in coreDataStack: CoreDataStack) {
         let userImageCache = UserImageLocalCache(location: nil)
-        let fileAssetCache = FileAssetCache()
+        let fileAssetCache = FileAssetCache(location: nil)
 
         coreDataStack.viewContext.zm_userImageCache = userImageCache
         coreDataStack.viewContext.zm_fileAssetCache = fileAssetCache
