9695ea0bacf4a58acda2e4341144890c3077cbd4
John Nguyen <28632506+johnxnguyen@users.noreply.github.com>
2022-03-02 15:56:00 +0100

    feat: store the current api version FS-451 (#491)
    * feat: add api versions to requests and responses FS-481
    
    * use ZMAPIVersion
    
    * feat: pass api version when asking for next request FS-451
    
    * feat: store the current api version FS-451
    
    * avoid default 0 value

---
+++ Sources/Helpers/ZMAPIVersion.swift
@@ -0,0 +1,69 @@
+// Wire
+// Copyright (C) 2022 Wire Swiss GmbH
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see http://www.gnu.org/licenses/.
+//
+
+import Foundation
+
+// MARK: - Current
+
+extension ZMAPIVersion {
+
+    private static let key = "currentAPIVersion"
+
+    /// The API version against which all new backend requests should be made.
+    ///
+    /// The current version should be the highest value in common between the set
+    /// of supported versions of the client (represented by `APIVersion` cases)
+    /// and the set of supported versions of the backend (obtainable via `GET /api-version`).
+    ///
+    /// A `nil` value indicates that no version is selected yet and therefore one
+    /// should be (re-)negotiated with the backend.
+
+    public static var current: Self? {
+        get {
+            // Fetching an integer will default to 0 if no value exists for the key,
+            // so explicitly check there is a value.
+            guard UserDefaults.standard.hasValue(for: key) else { return nil }
+            let storedValue = UserDefaults.standard.integer(forKey: key)
+            return ZMAPIVersion(rawValue: storedValue)
+        }
+
+        set {
+            UserDefaults.standard.set(newValue?.rawValue, forKey: key)
+        }
+    }
+
+}
+
+// MARK: - Comparable
+
+extension ZMAPIVersion: Comparable {
+
+    public static func < (lhs: Self, rhs: Self) -> Bool {
+        return lhs.rawValue < rhs.rawValue
+    }
+
+}
+
+// MARK: - Helper
+
+private extension UserDefaults {
+
+    func hasValue(for key: String) -> Bool {
+        return object(forKey: key) != nil
+    }
+
+}
