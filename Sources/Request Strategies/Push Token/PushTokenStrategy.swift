9f849dfd5f6a2d768e5e5709cda4d73f8950fabc
John Nguyen <28632506+johnxnguyen@users.noreply.github.com>
2022-05-16 22:45:17 +0200

    delete redundant code

--- Sources/Request Strategies/Push Token/PushTokenStrategy.swift
+++ Sources/Request Strategies/Push Token/PushTokenStrategy.swift
@@ -19,54 +19,22 @@
 import Foundation
 import WireDataModel
 
-let VoIPIdentifierSuffix = "-voip"
-let TokenKey = "token"
-let PushTokenPath = "/push/tokens"
+public class PushTokenStrategy: AbstractRequestStrategy, ZMEventConsumer {
 
-public class PushTokenStrategy: AbstractRequestStrategy, ZMUpstreamTranscoder, ZMContextChangeTrackerSource, ZMEventConsumer, ZMRequestGeneratorSource {
-
-    @objc public static let registerCurrentPushTokenNotificationName = Notification.Name(rawValue: "ZMUserSessionResetPushTokensNotification")
-
-    enum Keys {
-        static let UserClientPushTokenKey = "pushToken"
-        static let UserClientLegacyPushTokenKey = "legacyPushToken"
-        static let RequestTypeKey = "requestType"
-    }
-
-    enum RequestType: String {
-        case getToken
-    }
+    // MARK: - Properties
 
     private let registerPushTokenActionHandler: RegisterPushTokenActionHandler
     private let removePushTokenActionHandler: RemovePushTokenActionHandler
     private let getPushTokensActionHandler: GetPushTokensActionHandler
     private let actionSync: EntityActionSync
 
-    public var requestGenerators: [ZMRequestGenerator] {
-        return [
-            actionSync,
-            pushKitTokenSync
-        ]
-    }
-
-    fileprivate var pushKitTokenSync: ZMUpstreamModifiedObjectSync!
-    fileprivate var notificationsTracker: NotificationsTracker?
-
-    private func modifiedPredicate() -> NSPredicate {
-        guard let basePredicate = UserClient.predicateForObjectsThatNeedToBeUpdatedUpstream() else {
-            fatal("basePredicate is nil!")
-        }
-
-        let nonNilPushToken = NSPredicate(format: "%K != nil", Keys.UserClientPushTokenKey)
-
-        return NSCompoundPredicate(andPredicateWithSubpredicates: [basePredicate, nonNilPushToken])
-    }
+    // MARK: - Life cycle
 
     @objc
-    public init(withManagedObjectContext managedObjectContext: NSManagedObjectContext,
-                applicationStatus: ApplicationStatus,
-                analytics: AnalyticsType?) {
-
+    public override init(
+        withManagedObjectContext managedObjectContext: NSManagedObjectContext,
+        applicationStatus: ApplicationStatus
+    ) {
         registerPushTokenActionHandler = RegisterPushTokenActionHandler(context: managedObjectContext)
         removePushTokenActionHandler = RemovePushTokenActionHandler(context: managedObjectContext)
         getPushTokensActionHandler = GetPushTokensActionHandler(context: managedObjectContext)
@@ -78,116 +46,28 @@ public class PushTokenStrategy: AbstractRequestStrategy, ZMUpstreamTranscoder, Z
         ])
 
         super.init(withManagedObjectContext: managedObjectContext, applicationStatus: applicationStatus)
-        pushKitTokenSync = ZMUpstreamModifiedObjectSync(
-            transcoder: self,
-            entityName: UserClient.entityName(),
-            update: modifiedPredicate(),
-            filter: nil,
-            keysToSync: [Keys.UserClientPushTokenKey, Keys.UserClientLegacyPushTokenKey],
-            managedObjectContext: managedObjectContext
-        )
-
-        if let analytics = analytics {
-            self.notificationsTracker = NotificationsTracker(analytics: analytics)
-        }
     }
+
+    // MARK: - Requests
 
     public override func nextRequestIfAllowed(for apiVersion: APIVersion) -> ZMTransportRequest? {
-        return requestGenerators.nextRequest(for: apiVersion)
-    }
-
-// MARK: - ZMUpstreamTranscoder
-
-    public func request(forUpdating managedObject: ZMManagedObject, forKeys keys: Set<String>, apiVersion: APIVersion) -> ZMUpstreamRequest? {
-        guard let client = managedObject as? UserClient else { return nil }
-        guard client.isSelfClient() else { return nil }
-        guard let clientIdentifier = client.remoteIdentifier else { return nil }
-
-        let request: ZMTransportRequest
-        let requestType: RequestType
-
-        if let pushToken = client.pushToken, pushToken.isMarkedForDownload {
-            request = ZMTransportRequest(path: "\(PushTokenPath)", method: .methodGET, payload: nil, apiVersion: apiVersion.rawValue)
-            requestType = .getToken
-        } else {
-            return nil
-        }
-
-        return ZMUpstreamRequest(keys: keys,
-                                 transportRequest: request,
-                                 userInfo: [Keys.RequestTypeKey: requestType.rawValue])
-    }
-
-    public func request(forInserting managedObject: ZMManagedObject, forKeys keys: Set<String>?, apiVersion: APIVersion) -> ZMUpstreamRequest? {
-        return nil
-    }
-
-    public func updateInsertedObject(_ managedObject: ZMManagedObject, request upstreamRequest: ZMUpstreamRequest, response: ZMTransportResponse) {
-    }
-
-    public func updateUpdatedObject(_ managedObject: ZMManagedObject, requestUserInfo: [AnyHashable: Any]? = nil, response: ZMTransportResponse, keysToParse: Set<String>) -> Bool {
-        guard let client = managedObject as? UserClient else { return false }
-        guard client.isSelfClient() else { return false }
-        guard let userInfo = requestUserInfo as? [String: String] else { return false }
-        guard let requestTypeValue = userInfo[Keys.RequestTypeKey], let requestType = RequestType(rawValue: requestTypeValue) else { return false }
-
-        switch requestType {
-        case .getToken:
-            guard let pushToken = client.pushToken,
-                  let responseData = response.rawData else { return false }
-            guard let payload = try? JSONDecoder().decode([String: [PushTokenPayload]].self, from: responseData) else { return false }
-            guard let tokens = payload["tokens"] else { return false }
-
-            if tokens.first(where: { $0.client == client.remoteIdentifier && $0.token == pushToken.deviceTokenString }) != nil // We found one token that matches what we have locally
-            {
-                // Clear the flags and we are done
-                client.pushToken = pushToken.resetFlags()
-                return false
-            } else {
-                // There is something wrong, local token doesn't match the remotely registered
-
-                // We should remove the local token
-                client.pushToken = nil
-
-                notificationsTracker?.registerTokenMismatch()
-
-                // Make sure UI tries to get re-register a new one
-                NotificationInContext(name: Self.registerCurrentPushTokenNotificationName,
-                                      context: managedObjectContext.notificationContext,
-                                      object: nil,
-                                      userInfo: nil).post()
-
-                return false
-            }
-        }
-    }
-
-    public func objectToRefetchForFailedUpdate(of managedObject: ZMManagedObject) -> ZMManagedObject? {
-        return nil
-    }
-
-    public func shouldProcessUpdatesBeforeInserts() -> Bool {
-        return false
-    }
-
-    // MARK: - ZMContextChangeTrackerSource
-
-    public var contextChangeTrackers: [ZMContextChangeTracker] {
-        return [self.pushKitTokenSync]
+        return actionSync.nextRequest(for: apiVersion)
     }
 
     // MARK: - ZMEventConsumer
 
-    public func processEvents(_ events: [ZMUpdateEvent], liveEvents: Bool, prefetchResult: ZMFetchRequestBatchResult?) {
+    public func processEvents(
+        _ events: [ZMUpdateEvent],
+        liveEvents: Bool,
+        prefetchResult: ZMFetchRequestBatchResult?
+    ) {
         guard liveEvents else { return }
-
-        events.forEach { process(updateEvent: $0) }
+        events.forEach(process(updateEvent:))
     }
 
     func process(updateEvent event: ZMUpdateEvent) {
-        if event.type != .userPushRemove {
-            return
-        }
+        guard event.type == .userPushRemove else { return }
+
         // expected payload:
         // { "type: "user.push-remove",
         //   "token":
@@ -196,23 +76,7 @@ public class PushTokenStrategy: AbstractRequestStrategy, ZMUpstreamTranscoder, Z
         //          "token": "the token you get from apple"
         //    }
         // }
-        // we ignore the payload and remove the locally saved copy
-        let client = ZMUser.selfUser(in: self.managedObjectContext).selfClient()
-        client?.pushToken = nil
+
+        // TODO: what should we do here? Remove the local copy?
     }
-}
-
-private struct PushTokenPayload: Codable {
-
-    init(pushToken: PushToken, clientIdentifier: String) {
-        token = pushToken.deviceTokenString
-        app = pushToken.appIdentifier
-        transport = pushToken.transportType
-        client = clientIdentifier
-    }
-
-    let token: String
-    let app: String
-    let transport: String
-    let client: String
 }
