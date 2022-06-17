b052329303c9a261311ad3a1bbd577c1eab1cb30
Ghulam Mustafa <ghulam.mustafa@wire.com>
2022-06-17 15:53:19 +0200

    merging conflict bug fix

--- Sources/Request Strategies/MLS/MLSRequestStrategy.swift
+++ Sources/Request Strategies/MLS/MLSRequestStrategy.swift
@@ -32,7 +32,7 @@ public final class MLSRequestStrategy: AbstractRequestStrategy {
         applicationStatus: ApplicationStatus
     ) {
         entitySync = EntityActionSync(actionHandlers: [
-            CountSelfMLSKeyPackagesActionHandler(context: managedObjectContext)
+            CountSelfMLSKeyPackagesActionHandler(context: managedObjectContext),
             SendMLSWelcomeActionHandler(context: managedObjectContext),
             UploadSelfMLSKeyPackagesActionHandler(context: managedObjectContext),
             ClaimMLSKeyPackageActionHandler(context: managedObjectContext)
