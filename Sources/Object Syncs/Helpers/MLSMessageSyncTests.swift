9f021965d3b9bc78dca1397c2f2db003dafced56
John Nguyen <28632506+johnxnguyen@users.noreply.github.com>
2022-08-12 07:55:47 -0500

    add mls message sync tests

--- Sources/Object Syncs/Helpers/MLSMessageSyncTests.swift
+++ Sources/Object Syncs/Helpers/MLSMessageSyncTests.swift
@@ -21,10 +21,163 @@ import XCTest
 
 final class MLSMessageSyncTests: MessagingTestBase {
 
-    // test it notifies requests available
+    var sut: MLSMessageSync<MockOTREntity>!
+    var mockMLSController: MockMLSController!
+    var mockMessage: MockOTREntity!
 
-    // test request generation
+    override func setUp() {
+        super.setUp()
+        sut = MLSMessageSync(context: syncMOC)
+        mockMLSController = MockMLSController()
+        mockMessage = MockOTREntity(
+            messageData: Data([1, 2, 3]),
+            conversation: groupConversation,
+            context: syncMOC
+        )
 
-    // test response handling
+        syncMOC.performGroupedBlockAndWait {
+            self.groupConversation.mlsGroupID = MLSGroupID([1, 2, 3])
+            self.groupConversation.messageProtocol = .mls
+            self.syncMOC.test_setMockMLSController(self.mockMLSController)
+        }
+    }
+
+    override func tearDown() {
+        sut = nil
+        mockMLSController = nil
+        mockMessage = nil
+        super.tearDown()
+    }
+
+    // MARK: - Requests available
+
+    func test_ItNotifiesNewRequestAvailable_WhenSyncingMessage() {
+        // Expect
+        expectation(
+            forNotification: Notification.Name("RequestAvailableNotification"),
+            object: nil,
+            handler: nil
+        )
+
+        // When
+        sut.sync(mockMessage) { _, _ in
+            // No op
+        }
+
+        // Then
+        XCTAssertTrue(waitForCustomExpectations(withTimeout: 0.5))
+    }
+
+    // MARK: - Request generation
+
+    func test_RequestGeneration_Success() throws {
+        syncMOC.performGroupedBlockAndWait {
+            // When
+            let result = self.sut.transcoder.request(forEntity: self.mockMessage, apiVersion: .v2)
+
+            // Then
+            guard let request = result else {
+                XCTFail("no request generated")
+                return
+            }
+
+            XCTAssertEqual(request.path, "/v2/mls/messages")
+            XCTAssertEqual(request.method, .methodPOST)
+            XCTAssertEqual(request.binaryDataType, "message/mls")
+            XCTAssertEqual(request.apiVersion, 2)
+
+            let expectedEncryptedMessage = self.mockMessage.messageData + [000]
+            XCTAssertEqual(request.binaryData, expectedEncryptedMessage)
+        }
+    }
+
+    func test_RequestGeneration_Failure_OldAPIVersion() {
+        syncMOC.performGroupedBlockAndWait {
+            // Then
+            XCTAssertNil(self.sut.transcoder.request(
+                forEntity: self.mockMessage,
+                apiVersion: .v0
+            ))
+
+            XCTAssertNil(self.sut.transcoder.request(
+                forEntity: self.mockMessage,
+                apiVersion: .v1
+            ))
+        }
+    }
+
+    func test_RequestGeneration_Failure_ProteusProtocol() {
+        syncMOC.performGroupedBlockAndWait {
+            // Given
+            self.groupConversation.messageProtocol = .proteus
+
+            // Then
+            XCTAssertNil(self.sut.transcoder.request(
+                forEntity: self.mockMessage,
+                apiVersion: .v2
+            ))
+        }
+    }
+
+    func test_RequestGeneration_Failure_NoMLSGroupID() {
+        syncMOC.performGroupedBlockAndWait {
+            // Given
+            self.groupConversation.mlsGroupID = nil
+
+            // Then
+            XCTAssertNil(self.sut.transcoder.request(
+                forEntity: self.mockMessage,
+                apiVersion: .v2
+            ))
+        }
+    }
+
+    // MARK: - Response handling
+
+    func test_ResponseHandling_Success() {
+        // Given
+        let response = ZMTransportResponse(
+            payload: nil,
+            httpStatus: 201,
+            transportSessionError: nil,
+            apiVersion: 2
+        )
+
+        // When
+        sut.transcoder.request(
+            forEntity: mockMessage,
+            didCompleteWithResponse: response
+        )
+
+        // Then
+        XCTAssertTrue(mockMessage.isDelivered)
+    }
+
+    func test_ResponseHandling_Failure() {
+        // Given
+        let response = ZMTransportResponse(
+            payload: nil,
+            httpStatus: 409,
+            transportSessionError: nil,
+            apiVersion: 2
+        )
+
+        // When
+        sut.transcoder.request(
+            forEntity: mockMessage,
+            didCompleteWithResponse: response
+        )
+
+        // Then
+        XCTAssertFalse(mockMessage.isDelivered)
+    }
+
+}
+
+extension MockOTREntity: MLSMessage {
+
+    func encryptForTransport(using encrypt: (Data) throws -> Data) throws -> Data {
+        return try encrypt(messageData)
+    }
 
 }
